fad8ca3b6e44006710524840fc34b8bda71e7bf6
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-16 12:07:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  def initialize(word) 
+    @word = word 
+   
+ end 
+ def match(anagram_array)
+anagram_array.select do |w| (@word.chars.sort) == (w.chars.sort)
+end
+end
+end
+  
+    
+    
+    
