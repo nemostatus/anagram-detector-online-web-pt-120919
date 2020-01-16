169ce0ee44e892a532187a78e26cb04a5fe1d046
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-16 12:06:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,7 +2,7 @@ class Anagram
   attr_accessor :word 
   def initialize(word) 
     @word = word 
-   word.match 
+   
  end 
  def match(anagram_array)
 anagram_array.select do |w| (@word.chars.sort) == (w.charss.sort)
